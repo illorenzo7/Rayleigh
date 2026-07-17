52996bed2ce780a92836590755fc7f5edc892144
BJL <bjlazard@gmail.com>
2026-07-17 17:01:32 -0600

    forgot a comma

--- src/Physics/Controls.F90
+++ src/Physics/Controls.F90
@@ -105,7 +105,7 @@ Module Controls
     Namelist /Physical_Controls_Namelist/ magnetism, nonlinear, rotation, lorentz_forces, &
                 & viscous_heating, ohmic_heating, advect_reference_state, benchmark_mode, &
                 & benchmark_integration_interval, benchmark_report_interval, &
-                & momentum_advection, inertia, coriolis, centrifugal, gravity, remove_reference &
+                & momentum_advection, inertia, coriolis, centrifugal, gravity, remove_reference, &
                 & n_active_scalars, n_passive_scalars, &
                 & newtonian_cooling, newtonian_cooling_type, newtonian_cooling_time, &
                 & newtonian_cooling_tvar_amp, newtonian_cooling_profile_file, &
